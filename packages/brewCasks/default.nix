239ee475054ccb9e38d1c59a33462e9dd25968ca
Ágúst Hlynur Hólmgeirsson <aholmgeirsson@securecodewarrior.com>
2022-11-12 12:50:57 +0000

    Update jetbrains-toolbox arm override

--- packages/brewCasks/default.nix
+++ packages/brewCasks/default.nix
@@ -1,5 +1,7 @@
-{ lib, pkgs }:
-let
+{
+  lib,
+  pkgs,
+}: let
   caskApp = pkgs.callPackage ./caskApp.nix;
   casks = lib.importJSON ./cask.json;
   # Some apps do not contain a valid sha256 hash
@@ -7,22 +9,34 @@ let
 
   # Current JSON only contains x86 variants, quickfix to support arm
   armOverrides =
-    if pkgs.system == "aarch64-darwin" then {
+    if pkgs.system == "aarch64-darwin"
+    then {
       ferdi = {
         sha256 = "ec7ccceba08f1c581290d6ce4f5fa5478bed2c713c592d0298856f7b2719f35d";
         url = "https://github.com/getferdi/ferdi/releases/download/v5.8.1/Ferdi-5.8.1-arm64.dmg";
       };
-jetbrains-toolbox = {
-sha256 = "bc0564476d767841a85701e2b2a7ee5d28688ee87bb381cc4172612e6938a71a";
-url = "https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.24.12080-arm64.dmg";
-};
+      jetbrains-toolbox = {
+        sha256 = "7320815edc66d35f50c110b11266bed6190250201bb37f2413e90b56307686c5";
+        url = "https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.26.5.13419-arm64.dmg";
+      };
+    }
+    else {};
 
-    } else { };
-
-  apps = builtins.listToAttrs (builtins.map (c: { name = c.token; value = c; }) validCasks);
+  apps = builtins.listToAttrs (builtins.map (c: {
+      name = c.token;
+      value = c;
+    })
+    validCasks);
 in
-lib.attrsets.mapAttrs
-  (name: value: caskApp {
-    cask = value // (if builtins.hasAttr name armOverrides then armOverrides.${name} else { });
-  })
+  lib.attrsets.mapAttrs
+  (name: value:
+    caskApp {
+      cask =
+        value
+        // (
+          if builtins.hasAttr name armOverrides
+          then armOverrides.${name}
+          else {}
+        );
+    })
   apps
